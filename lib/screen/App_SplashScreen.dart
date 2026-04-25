9360a706140856ae987f2730fb5aef6c267fe3b9
Ahmed <84560856+atarekgg@users.noreply.github.com>
2026-04-25 17:45:36 +0300

    Initial commit

--- lib/screen/App_SplashScreen.dart
+++ lib/screen/App_SplashScreen.dart
@@ -1,9 +1,9 @@
+// lib/screen/splash_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_flutter_task2/assets/app_images.dart';
-import 'package:my_flutter_task2/bloc/calculator_bloc.dart';
+import 'package:my_flutter_task2/Bloc/calculator_bloc.dart'; 
 import 'package:my_flutter_task2/screen/calculator_screen.dart';
-
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,33 +15,33 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
-  super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => BlocProvider( 
-        create: (context) => CalculatorBloc(),
-        child: const CalculatorScreen(), 
-      ),),
-    );
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => BlocProvider(
+              create: (context) => CalculatorBloc(),
+              child: const CalculatorScreen(),
+            ),
+          ),
+        );
+      }
+    });
+  }
 
-  });
-}
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(AppImages.SplashScreenbg),
             fit: BoxFit.cover,
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-
-          ],
         ),
       ),
     );
